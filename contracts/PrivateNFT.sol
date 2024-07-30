a11efe5f3d914f376dea1f9f6da828bae5d28876
Badal Kumar <156704886+dsthgsrtzdfg@users.noreply.github.com>
2024-07-30 10:05:49 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("git add . && git commit -m "feat: initiated the project" && git push origin main","")
+        ERC721("thrt","hf")
         Ownable(initialOwner)
     {}
 
